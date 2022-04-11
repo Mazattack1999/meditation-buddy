548aad8b9bebfb4c194d63ffac6c4df5c70e58a9
micah_z <micah.a.zahn@gmail.com>
2022-04-11 09:47:38 -0500

    add fetch requests

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,71 @@
-categories = ["nature", "space", "dogs", ];
+var categories = ["nature", "space", "animals", ];
+var pApiKey = "563492ad6f917000010000015d8387ef83034b6792272314a7fbbf6b";
+// if inital page load has completed or not
+var started = false;
+
+// fetch data
+var imageData;
+var quoteData;
+
+//homepage header
+var homeHeader = document.querySelector("#index-header");
+
+function storeImages(value){
+    fetch("https://api.pexels.com/v1/search?query=" + value + "&per_page=25", {
+        headers: {
+            Authorization: pApiKey
+        }
+    })
+    .then(function (response){
+        return response.json()
+    }).then(function(data){
+        imageData = data.photos;
+
+        // check if page is not started and quote data is not null
+        if (!started && quoteData) {
+            started = true;
+            loadHomePage();
+        }
+        
+    })
+}
+
+function storeQuotes() {
+    const url = "https://zenquotes.io/api/quotes";
+    const proxy = "https://octoproxymus.herokuapp.com/?secret=walrus&url=";
+    fetch(proxy + url)
+        .then(function (response){
+            return  response.json() 
+        })
+    .then( function (data){
+        var quoteData = data;
+
+        // check if page is not started and image data is not null
+        if (!started && imageData) {
+            started = true;
+            loadHomePage();
+        }
+    })
+ }
+
+ // get a random value from any array
+ function getRandom(array){
+     // random index
+     var randIndex = Math.floor(Math.random() * array.length);
+     return randIndex;
+ }
+
+
+ function loadStartupData() {
+    storeImages(getRandom(categories));
+ }
+
+ // must execute after asynchronous functionality of fetches is complete
+ function loadHomePage() {
+     // fetch and store images of random category
+     storeImages(getRandom(categories));
+    var randomImage = getRandom(imageData);
+    console.log(randomImage);
+ }
+
+loadHomePage();
